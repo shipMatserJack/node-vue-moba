ef574639fabee3e00d0757b7f1745c5703eb593d
huangjie <841440416@qq.com>
2020-05-17 20:43:09 +0800

    *技能编辑完成

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -92,8 +92,11 @@
               <el-form-item label="描述">
                 <el-input type="textarea" v-model="item.description"></el-input>
               </el-form-item>
-              <el-form-item label="提示">
+              <el-form-item label="小提示">
                 <el-input type="textarea" v-model="item.tips"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" type="danger" @click="model.skills.splice(i, 1)">删除</el-button>
               </el-form-item>
             </el-col>
           </el-row>
--- server/models/Hero.js
+++ server/models/Hero.js
@@ -14,7 +14,7 @@ const schema = new mongoose.Schema({
   skills: [{
     icon: {type: String},
     name: {type: String},
-    discription: {type: String},
+    description: {type: String},
     tips: {type: String}
   }],
   items1: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Item'}],
